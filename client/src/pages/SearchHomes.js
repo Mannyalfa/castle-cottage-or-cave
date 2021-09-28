cc691ba5ab1b1c0ecea2eb6e72d2dca9e9871553
Menji <jennifer.menjivar18@gmail.com>
2021-09-28 17:30:30 -0400

    created select menus for beds, baths, and state

--- client/src/pages/SearchHomes.js
+++ client/src/pages/SearchHomes.js
@@ -100,61 +100,139 @@ const SearchHomes = () => {
             <Form.Row>
               <Col xs={10} md={12} lg={12} className="searchBar">
                 <Row>
-                  <Col xs={8} md={3} lg={5} className="searchBarElement">
+                  <Col xs={8} md={3} className="searchBarElement">
                     <Form.Control
                       name="city"
                       value={city}
                       onChange={(e) => setCity(e.target.value)}
                       type="text"
-                      size="lg"
+                      size="md"
                       placeholder="Enter a City"
                     />
                   </Col>
-                  <Col xs={4} md={2} lg={2} className="searchBarElement">
+                  <Col xs={4} md={2} className="searchBarElement">
                     <Form.Control
                       name="stateId"
+                      as="select"
                       value={stateId}
                       onChange={(e) => setStateId(e.target.value)}
                       type="text"
-                      size="lg"
-                      placeholder="State"
-                    />
+                      size="md"
+                      placeholder="St"
+                    >
+                      <option key="blankChoice" value="0">
+                        State
+                      </option>
+                      <option value="AL">Alabama</option>
+                      <option value="AK">Alaska</option>
+                      <option value="AZ">Arizona</option>
+                      <option value="AR">Arkansas</option>
+                      <option value="CA">California</option>
+                      <option value="CO">Colorado</option>
+                      <option value="CT">Connecticut</option>
+                      <option value="DE">Delaware</option>
+                      <option value="DC">District Of Columbia</option>
+                      <option value="FL">Florida</option>
+                      <option value="GA">Georgia</option>
+                      <option value="HI">Hawaii</option>
+                      <option value="ID">Idaho</option>
+                      <option value="IL">Illinois</option>
+                      <option value="IN">Indiana</option>
+                      <option value="IA">Iowa</option>
+                      <option value="KS">Kansas</option>
+                      <option value="KY">Kentucky</option>
+                      <option value="LA">Louisiana</option>
+                      <option value="ME">Maine</option>
+                      <option value="MD">Maryland</option>
+                      <option value="MA">Massachusetts</option>
+                      <option value="MI">Michigan</option>
+                      <option value="MN">Minnesota</option>
+                      <option value="MS">Mississippi</option>
+                      <option value="MO">Missouri</option>
+                      <option value="MT">Montana</option>
+                      <option value="NE">Nebraska</option>
+                      <option value="NV">Nevada</option>
+                      <option value="NH">New Hampshire</option>
+                      <option value="NJ">New Jersey</option>
+                      <option value="NM">New Mexico</option>
+                      <option value="NY">New York</option>
+                      <option value="NC">North Carolina</option>
+                      <option value="ND">North Dakota</option>
+                      <option value="OH">Ohio</option>
+                      <option value="OK">Oklahoma</option>
+                      <option value="OR">Oregon</option>
+                      <option value="PA">Pennsylvania</option>
+                      <option value="RI">Rhode Island</option>
+                      <option value="SC">South Carolina</option>
+                      <option value="SD">South Dakota</option>
+                      <option value="TN">Tennessee</option>
+                      <option value="TX">Texas</option>
+                      <option value="UT">Utah</option>
+                      <option value="VT">Vermont</option>
+                      <option value="VA">Virginia</option>
+                      <option value="WA">Washington</option>
+                      <option value="WV">West Virginia</option>
+                      <option value="WI">Wisconsin</option>
+                      <option value="WY">Wyoming</option>
+                    </Form.Control>
                   </Col>
-                  <Col xs={3} md={2} lg={1} className="searchBarElement">
+                  <Col xs={3} md={2} className="searchBarElement">
                     <Form.Control
                       name="bed"
+                      as="select"
                       value={bed}
                       onChange={(e) => setBed(e.target.value)}
                       type="text"
                       pattern="[0-9]*"
-                      size="lg"
+                      size="md"
                       placeholder="Beds"
-                    />
+                    >
+                      <option key="blankChoice" value="0">
+                        Beds
+                      </option>
+                      <option value="0">0</option>
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                      <option value="5">5+</option>
+                    </Form.Control>
                   </Col>
-                  <Col xs={3} md={2} lg={1} className="searchBarElement">
+                  <Col xs={3} md={2} className="searchBarElement">
                     <Form.Control
                       name="bath"
+                      as="select"
                       value={bath}
                       onChange={(e) => setBath(e.target.value)}
                       type="text"
                       pattern="[0-9]*"
-                      size="lg"
+                      size="md"
                       placeholder="Baths"
-                    />
+                    >
+                      <option key="blankChoice" value="0">
+                        Baths
+                      </option>
+                      <option value="0">0</option>
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                      <option value="5">5+</option>
+                    </Form.Control>
                   </Col>
-                  <Col xs={4} md={2} lg={2} className="searchBarElement">
+                  <Col xs={4} md={2} className="searchBarElement">
                     <Form.Control
                       name="rent_max"
                       pattern="[0-9]*"
                       value={rent_max}
                       onChange={(e) => setRentMax(e.target.value)}
                       type="text"
-                      size="lg"
+                      size="md"
                       placeholder="Max Rent"
                     />
                   </Col>
                   <Col xs={1} className="searchBarElement">
-                    <Button type="submit" variant="dark" size="lg">
+                    <Button type="submit" variant="dark" size="md">
                       <FaSearch />
                     </Button>
                   </Col>
